45ea75a318ca441f48776484503d152fd7b9fbe1
Achyuth KP <kpachyuthz@gmail.com>
2023-02-17 19:23:35 +0530

    Introducing newer version

--- assets/js/tindex_0.2.js
+++ assets/js/tindex_0.2.js
@@ -57,7 +57,10 @@
   let preloader = select("#preloader");
   document.getElementById("contact-us").innerHTML +=
     '<section id="subscribe"> <div class="container" data-aos="fade-up"> <div class="row"> <div class="col-md-5"> <img src="assets/img/Services/contact-us.png" alt="" class="img-fluid"> </div> <div class="col-md-7 subscribe-btn-container"> <h3 class="subscribe-title">Contact Us For Updates</h3> <p class="subscribe-text">Contact Tekspy for more information on our background check services.</p> <a class="subscribe-btn" href="contact">Contact Us</a> </div> </div> </div> </section>';
-
+  document.getElementById("quick-links").innerHTML +=
+    '<ul> <li><i class="bi bi-chevron-right"></i> <a href="Resume">Resume Validation</a></li> <li><i class="bi bi-chevron-right"></i> <a href="index">Home</a></li> <li><i class="bi bi-chevron-right"></i> <a href="About">About us</a></li> <li><i class="bi bi-chevron-right"></i> <a href="Why-Us">Why us</a></li> <li><i class="bi bi-chevron-right"></i> <a href="CaseStudy">Case Study</a></li> <li><i class="bi bi-chevron-right"></i> <a href="contact">Contact</a></li> </ul>';
+  document.getElementById("service-links").innerHTML +=
+    '<ul> <li><i class="bi bi-chevron-right"></i> <a href="Education">Education Check</a></li> <li><i class="bi bi-chevron-right"></i> <a href="Reference">Reference Check</a></li> <li><i class="bi bi-chevron-right"></i> <a href="Employment">Employment Check</a></li> <li><i class="bi bi-chevron-right"></i> <a href="Address">Address Check</a></li> <li><i class="bi bi-chevron-right"></i> <a href="Criminal">Criminal Check</a></li> <li><i class="bi bi-chevron-right"></i> <a href="SocialMedia">Social Media Check</a></li> </ul>';
   function aos_init() {
     AOS.init({ duration: 1e3, easing: "ease-in-out", once: !0, mirror: !1 });
   }
