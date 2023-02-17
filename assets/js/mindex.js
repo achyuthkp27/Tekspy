e5d9abf68613206c1eb8a3a8f97d9be5c26a0160
Achyuth KP <kpachyuthz@gmail.com>
2023-02-17 17:33:58 +0530

    Fixing CDN

--- assets/js/mindex.js
+++ assets/js/mindex.js
@@ -28,6 +28,34 @@
     window.addEventListener("load", headerFixed),
       onscroll(document, headerFixed);
   }
+  (modal = document.querySelector("#myModal")),
+    (btn = document.querySelectorAll(".btn-client")),
+    (closeModal = document.getElementsByClassName("close")[0]);
+  for (let i = 0; i < btn.length; i++)
+    btn[i].addEventListener("click", function () {
+      modal.style.display = "block";
+    });
+  (btn.onclick = function () {
+    modal.style.display = "block";
+  }),
+    (closeModal.onclick = function () {
+      modal.style.display = "none";
+    }),
+    (window.onclick = function (event) {
+      event.target == modal && (modal.style.display = "none");
+    });
+  const typed = select(".typed");
+  if (typed) {
+    let typed_strings = typed.getAttribute("data-typed-items");
+    (typed_strings = typed_strings.split(",")),
+      new Typed(".typed", {
+        strings: typed_strings,
+        loop: !0,
+        typeSpeed: 100,
+        backSpeed: 50,
+        backDelay: 2e3,
+      });
+  }
   let backtotop = select(".back-to-top");
   if (backtotop) {
     const toggleBacktotop = () => {
